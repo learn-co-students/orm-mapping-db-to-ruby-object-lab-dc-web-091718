edce2099324f1bdf93b866fda91cdc8c203e6908
serpentz <absalomcherinet@gmail.com>
2018-10-01 12:01:05 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,18 +1,88 @@
 class Student
   attr_accessor :id, :name, :grade
 
+  
+
   def self.new_from_db(row)
     # create a new Student object given a row from the database
+    stud = Student.new
+    stud.id = row[0]
+    stud.name = row[1]
+    stud.grade = row[2]
+    stud
   end
 
   def self.all
     # retrieve all the rows from the "Students" database
     # remember each row should be a new instance of the Student class
+    sql = <<-SQL
+    SELECT * FROM students
+    SQL
+    array = []
+    DB[:conn].execute(sql).each do |row|
+      array<<self.new_from_db(row)
+    end
+    array
   end
 
-  def self.find_by_name(name)
-    # find the student in the database given a name
-    # return a new instance of the Student class
+  def self.all_students_in_grade_X(x)
+
+    sql = <<-SQL
+        SELECT * FROM students WHERE grade  = ?  
+    SQL
+    arr = []
+    DB[:conn].execute(sql,10).each do |row|
+        arr<<self.new_from_db(row)
+      end
+    arr
+
+  end
+
+  def self.first_student_in_grade_10
+    sql = <<-SQL
+        SELECT * FROM students WHERE grade = 10 LIMIT ? 
+    SQL
+    arr = []
+    new_from_db(DB[:conn].execute(sql,1)[0])
+
+  end
+
+  def self.first_X_students_in_grade_10(num)
+    sql = <<-SQL
+        SELECT * FROM students WHERE grade  = 10 LIMIT ? 
+    SQL
+    arr = []
+    DB[:conn].execute(sql,2).each do |row|
+        arr<<self.new_from_db(row)
+      end
+    arr
+  end
+
+  def self.students_below_12th_grade
+    sql = <<-SQL
+        SELECT * FROM students WHERE grade < ? 
+    SQL
+    arr = []
+    DB[:conn].execute(sql,12).each do |row|
+        arr<<self.new_from_db(row)
+      end
+    arr
+  end
+
+  def self.all_students_in_grade_9
+    sql = <<-SQL
+        SELECT * FROM students WHERE grade= ? 
+    SQL
+    arr = []
+    DB[:conn].execute(sql,9).each do |row|
+        arr<<self.new_from_db(row)
+      end
+    arr
+  end
+
+  def self.find_by_name(name_param)
+    sql = "SELECT * FROM students WHERE name = ?"
+    Student.new_from_db(DB[:conn].execute(sql, name_param)[0])
   end
   
   def save
